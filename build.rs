f1f898509a6c283aee5a585f802b822a15595958
Julian Tepper <Juliantepper2006+github@gmail.com>
2025-02-22 15:32:20 +0100

    fixed build.rs to run on linux

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
-extern crate windows_exe_info;
-
 #[cfg(target_os = "windows")]
 fn main() {
+    extern crate windows_exe_info;
     windows_exe_info::icon::icon_ico(std::path::Path::new("./images/logo.ico"));
 }
 
